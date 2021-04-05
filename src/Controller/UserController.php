1c59c7cf819195ea52f15cc3b60049d52a12c7e9
Yasmine <yasmine.bouslimani@gmail.com>
2021-04-05 20:33:37 +0200

    Add permissions managment

---
+++ src/Controller/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Model\UserManager;
+
+class UserController extends AbstractController
+{
+
+    public function index()
+    {
+        $userManager = new UserManager();
+        $inactiveUsers = $userManager->selectAllInactiveUsers();
+        $activeUsers = $userManager->selectAllActiveUsers();
+
+        return $this->twig->render('User/index.html.twig', [
+            'inactiveUsers' => $inactiveUsers,
+            'activeUsers' => $activeUsers
+        ]);
+    }
+
+
+    public function show($id)
+    {
+        $userManager = new UserManager();
+        $user = $userManager->getUserWithEmployeeById($id);
+        $permissions = json_decode($user['permissions']);
+
+        return $this->twig->render('User/show.html.twig', [
+            'user' => $user,
+            'permissions' => $permissions
+        ]);
+    }
+
+
+    public function edit(int $id)
+    {
+        $userManager = new UserManager();
+        $user = $userManager->getUserWithEmployeeById($id);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $user['login'] = $_POST['login'];
+            $user['is_active'] = $_POST['is_active'];
+            $user['permissions'] = json_encode($_POST['permissions']);
+            $userManager->updateUser($user);
+
+            header('Location: /user/index/');
+        }
+
+        return $this->twig->render('User/edit.html.twig', [
+                'user' => $user,
+            ]
+        );
+    }
+
+}
